9c015b2697e5c81a6c1696893bcf175f130762a1
Asmar <asmarcd@gmail.com>
2020-09-30 17:17:43 -0700

    Initial file push

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+
+// npm init DONE
+// install my dependenices - inquirer DONE
+// create all the dependencies I'll need - inquirer, fs
+// create an array of questions
+// write a readme in a markdown file as a template
+// function that will generate my readme template
+// use inquirer to prompt users with questions
+// use answers that come back from inquirer
+// use answers that come back from inquirer, pass those into my generate readme function
+// write file using template generated from readme function
